f91294e545c73f9b7efb6fb836dc96b0e0595a0a
Pheakdey <frapperpnext@gmail.com>
2023-09-05 22:16:20 +0700

    update epos

--- epos_restaurant_2023/inventory/doctype/product_category/product_category.js
+++ epos_restaurant_2023/inventory/doctype/product_category/product_category.js
@@ -1,8 +1,17 @@
 // Copyright (c) 2022, Tes Pheakdey and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Product Category", {
-// 	refresh(frm) {
+frappe.ui.form.on("Product Category", {
+	refresh(frm) {
 
-// 	},
-// });
+	},
+    setup(frm){
+        frm.set_query('parent_product_category', () => {
+            return {
+                filters: {
+                    is_group: 0
+                }
+            }
+        });
+    },
+});
