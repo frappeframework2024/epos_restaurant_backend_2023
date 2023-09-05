27a1423cfd66ae25e530792b0e3bc919489e22df
Pheakdey <frapperpnext@gmail.com>
2023-09-05 20:49:12 +0700

    update epos

--- epos_restaurant_2023/www/product_detail.py
+++ epos_restaurant_2023/www/product_detail.py
@@ -1,5 +1,15 @@
 import frappe
+from py_linq import Enumerable
 def get_context(context):
  
     product = frappe.get_doc("Product",frappe.form_dict.product_code)
+    min_price = product.price or 0
+    max_price = product.price or 0
+    
+    if len(product.product_price)>0:
+        min_price = Enumerable(product.product_price).min(lambda x: x.price or 0)
+        max_price = Enumerable(product.product_price).max(lambda x: x.price or 0)
+
     context.doc = product
+    context.min_price = min_price
+    context.max_price = max_price
