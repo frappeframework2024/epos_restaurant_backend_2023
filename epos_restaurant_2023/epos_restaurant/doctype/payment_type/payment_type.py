c0553d9e9c5e88c427125e49dd166acca7accd8c
outdomleap <outdom.leap@esoftix.com>
2024-02-09 10:18:11 +0700

    update

--- epos_restaurant_2023/epos_restaurant/doctype/payment_type/payment_type.py
+++ epos_restaurant_2023/epos_restaurant/doctype/payment_type/payment_type.py
@@ -8,18 +8,21 @@ class PaymentType(Document):
 	def validate(self):
 		if self.currency == frappe.db.get_default("currency"):
 			self.exchange_rate = 1
+			self.change_exchange_rate = 1
 		else: 
 			main_exchange_currency = frappe.db.get_default("exchange_rate_main_currency")
-			sql = "select exchange_rate from `tabCurrency Exchange` where to_currency='{}' order by posting_date desc limit 1".format(self.currency)
+			sql = "select exchange_rate,change_exchange_rate from `tabCurrency Exchange` where to_currency='{}' order by posting_date desc limit 1".format(self.currency)
 			if self.currency == main_exchange_currency:
-				sql = "select exchange_rate from `tabCurrency Exchange` where from_currency='{}' and to_currency = '{}'  order by posting_date desc limit 1".format(self.currency, frappe.db.get_default("currency"))
+				sql = "select exchange_rate,change_exchange_rate from `tabCurrency Exchange` where from_currency='{}' and to_currency = '{}'  order by posting_date desc limit 1".format(self.currency, frappe.db.get_default("currency"))
 
 			data = frappe.db.sql(sql)
 
 			if data:
 				self.exchange_rate = data[0][0] or 1
+				self.change_exchange_rate = data[0][0] or 1
 			else:
 				self.exchange_rate  =1 
+				self.change_exchange_rate  =1 
 		
 	def on_update(self):
 		if self.has_value_changed("payment_type_group"):
