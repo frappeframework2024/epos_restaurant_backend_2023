2dc8c4606cd869a28b7cbda8c95d0fd67ba9be78
outdomleap <outdom.leap@esoftix.com>
2024-03-02 10:51:31 +0700

    upate

--- epos_restaurant_2023/employee_management/report/employee_time_sheet_summary/employee_time_sheet_summary.js
+++ epos_restaurant_2023/employee_management/report/employee_time_sheet_summary/employee_time_sheet_summary.js
@@ -92,9 +92,9 @@ frappe.query_reports["Employee Time Sheet Summary"] = {
 	],
 	"formatter": function(value, row, column, data, default_formatter) {	
 		value = default_formatter(value, row, column, data);
-		console.log(column)
+		// console.log(data.employee_code)
 		if (column.fieldname=="employee_name") {
-			value = $(`<span><a onclick="epos_restaurant_2023.employee_time_sheet_summary.open_report('${value}')" href='/printview?doctype=Employee&name=${value}&format=Employee%20Time%20Sheet&no_letterhead=1&letterhead=No%20Letterhead&settings=%7B%7D&_lang=en'>${value}</a></span>`);
+			value = $(`<span><a onclick="epos_restaurant_2023.employee_time_sheet_summary.open_report('${value}')" href='/printview?doctype=Employee&name=${data.employee_code}&format=Employee%20Time%20Sheet&no_letterhead=1&letterhead=No%20Letterhead&settings=%7B%7D&_lang=en&trigger_print=1'>${value}</a></span>`);
 
 			var $value = $(value).css("font-weight", "bold");
 			// value = "<a target='_blank' href='http://192.168.10.19:1216/printview?doctype=Business%20Branch&name=ESTC%20HOTEL&format=Front%20Desk%20Arrival%20Guest%20Report&&settings=%7B%7D&show_toolbar=0&start_date=2024-02-29&end_date=2024-03-06&_lang=en&refresh=14.15485363871575'"+ "</a>";
