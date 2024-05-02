50e5519796ab19b602c780f0f41f0dcfa616097f
outdomleap <outdom.leap@esoftix.com>
2024-05-02 16:14:52 +0700

    update ratha 2024-05-02 4:14 PM

--- epos_restaurant_2023/selling/doctype/bulk_sale_payment/bulk_sale_payment.js
+++ epos_restaurant_2023/selling/doctype/bulk_sale_payment/bulk_sale_payment.js
@@ -2,25 +2,39 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Bulk Sale Payment", {
+    setup(frm){
+        frm.set_query("sale","sale_list", function() {
+            return {
+                filters: {
+                    balance: ['>', 0],
+                    docstatus: ['=', 1]
+                }
+            }
+        });
+    },
 	refresh(frm) {
-
+        updatetotal(frm);
 	},
     customer(frm) {
 		if (frm.doc.customer) {
+            frm.set_value('sale_list', []);
 			frappe.call({
                 method: "epos_restaurant_2023.selling.doctype.bulk_sale_payment.bulk_sale_payment.get_sale_by_customer",
                 args: {
-                    customer:frm.doc.customer,
-                    payment_type:frm.doc.payment_type
+                    customer:frm.doc.customer
                 },
                 callback: function(r){
                     r.message.forEach((r => {
                         doc = frm.add_child("sale_list");
                         doc.sale = r.sale;
-                        doc.amount = r.amount;
-                        doc.payment_type = r.payment_type;
-                        doc.payment_amount = r.balance == r.amount ? r.amount : r.payment_amount;
-                        doc.balance = r.balance == r.amount ? 0 : r.balance;
+                        doc.amount = r.balance;
+                        doc.payment_type = frm.doc.payment_type;
+                        doc.currency = frm.doc.currency;
+                        doc.exchange_rate = frm.doc.exchange_rate;
+                        doc.input_amount = r.balance * doc.exchange_rate;
+                        doc.payment_amount = r.balance ;
+                        doc.balance = 0;
+                        doc.posting_date = frm.doc.posting_date
                     }))
                     frm.refresh_field('sale_list');
                     updatetotal(frm);
@@ -28,7 +42,46 @@ frappe.ui.form.on("Bulk Sale Payment", {
             });
 		}
 	},
+    payment_type(frm){ 
+        if(frm.doc.sale_list.length > 0){
+            $.each(frm.doc.sale_list, function(i, d) {
+                d.payment_type = frm.doc.payment_type;
+                d.currency = frm.doc.currency;
+                d.exchange_rate = (frm.doc.exchange_rate || 0);
+                d.input_amount = d.amount * (d.exchange_rate || 0);
+                d.payment_amount = d.input_amount == 0 ? 0 : d.input_amount /  (d.exchange_rate || 0);
+                d.balance = d.amount - d.payment_amount;
+                frm.refresh_field('sale_list');
+                updatetotal(frm);
+            });
+        }
+    },
 });
+
+frappe.ui.form.on('Bulk Sale', {
+    input_amount(frm, cdt, cdn) {
+		let doc = locals[cdt][cdn];
+        doc.payment_amount = doc.input_amount / doc.exchange_rate
+        if(doc.payment_amount>doc.amount){
+            doc.payment_amount = doc.amount
+            doc.input_amount = doc.amount * doc.exchange_rate
+        }
+        doc.balance = doc.amount - doc.payment_amount
+        frm.refresh_field('sale_list');
+        updatetotal(frm);
+	},
+    sale(frm,cdt,cdn){
+        let doc = locals[cdt][cdn];
+        doc.payment_type = frm.doc.payment_type;
+        doc.currency = frm.doc.currency;
+        doc.exchange_rate = (frm.doc.exchange_rate || 0);
+        doc.input_amount = doc.amount * (doc.exchange_rate || 0);
+        doc.payment_amount = doc.input_amount == 0 ? 0 : doc.input_amount /  (doc.exchange_rate || 0);
+        doc.balance = doc.amount - doc.payment_amount;
+        frm.refresh_field('sale_list');
+        updatetotal(frm);
+    }
+})
 
 function updatetotal(frm){
     const sales = frm.doc.sale_list;
@@ -38,4 +91,5 @@ function updatetotal(frm){
     frm.set_value('total_amount', sales.reduce((n, d) => n + d.amount, 0));
 	frm.set_value('total_payment_amount', sales.reduce((n, d) => n + d.payment_amount, 0));
 	frm.set_value('total_balance', sales.reduce((n, d) => n + d.balance, 0));
+	frm.set_value('total_sale', sales.reduce((n, d) => n + 1, 0));
 }
--- epos_restaurant_2023/selling/doctype/bulk_sale_payment/bulk_sale_payment.py
+++ epos_restaurant_2023/selling/doctype/bulk_sale_payment/bulk_sale_payment.py
@@ -5,10 +5,67 @@ import frappe
 from frappe.model.document import Document
 
 
-class BulkSalePayment(Document):
-	pass
+class BulkSalePayment(Document):	
+	def on_submit(self):
+		if len(self.sale_list) <= 20:
+			add_sale_payment_enqueue(self)
+		else:	
+			frappe.enqueue(add_sale_payment_enqueue,queue="short",doc=self)
+		self.reload()
+		
+	def on_cancel(self):
+		if len(self.sale_list) <= 20:
+			on_cancel_enqueue(self)
+		else:
+			frappe.enqueue(on_cancel_enqueue,queue="short",doc=self)
+		self.reload()
+
+def on_cancel_enqueue(doc):
+	for d in doc.sale_list:
+			cancel_sale_payment(d)
+
+def add_sale_payment_enqueue(doc):
+	for d in doc.sale_list:
+			add_sale_payment(d)
+
+def cancel_sale_payment(d):
+	doc = frappe.get_doc('Sale Payment', d.sale_payment)
+	doc.cancel()
 
 @frappe.whitelist()
-def get_sale_by_customer(customer,payment_type):
-	sales = frappe.db.sql("select name sale,'{1}' payment_type,grand_total amount,total_paid payment_amount,balance from `tabSale` where customer = '{0}' and docstatus=1 and balance > 0".format(customer,payment_type),as_dict=1)
+def get_sale_by_customer(customer):
+	sales = frappe.db.sql("select name sale,grand_total amount,total_paid payment_amount,balance from `tabSale` where customer = '{0}' and docstatus = 1 and balance > 0".format(customer),as_dict=1)
 	return sales
+
+def add_sale_payment(doc):
+	p = frappe.new_doc("Sale Payment")
+	p.posting_date = doc.posting_date
+	p.payment_type = doc.payment_type
+	p.currency = doc.currency
+	p.exchange_rate = doc.exchange_rate
+	p.payment_amount = doc.payment_amount
+	p.input_amount = doc.input_amount
+	p.sale = doc.sale
+	p.note = doc.note
+	p.insert()
+	p.submit()
+
+	bs = frappe.get_doc("Bulk Sale",doc.name)
+	bs.sale_payment = p.name
+	bs.save()
+
+	update_sale_balance(doc)
+
+
+def update_sale_balance(doc):
+	currency_precision = frappe.db.get_single_value('System Settings', 'currency_precision')
+	if currency_precision=='':
+			currency_precision = "2"
+	if doc.sale:
+		data = frappe.db.sql("select  ifnull(sum(payment_amount),0)  as total_paid from `tabSale Payment` where docstatus=1 and sale='{}' and payment_amount>0".format(doc.sale))
+		sale_amount = frappe.db.get_value('Sale', doc.sale, 'grand_total')
+		if data and sale_amount:
+			balance =round(sale_amount,int(currency_precision))-round(data[0][0], int(currency_precision))  
+			if balance<0:
+				balance = 0
+			frappe.db.set_value('Sale', doc.sale,  {'total_paid': round(data[0][0],int(currency_precision)),'balance': balance})
