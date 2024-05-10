8d52674e3f3d139fbbbc74e89d6d8bfe3c2809c4
outdomleap <outdom.leap@esoftix.com>
2024-05-10 10:22:18 +0700

    update

--- epos_frontend/src/views/report/Report.vue
+++ epos_frontend/src/views/report/Report.vue
@@ -387,7 +387,11 @@ function onExport(){
 
  
 function onPrint(){ 
-    gv.onPrintWorkingDayAndCashierShift(activeReport.value.report_id,pos_profile,activeReport.value.doc_type)
+    
+    if(activeReport.value.doc_type == "Cashier Shift" || activeReport.value.doc_type == "Working Day"){
+        gv.onPrintWorkingDayAndCashierShift(activeReport.value.report_id,pos_profile,activeReport.value.doc_type)
+    }
+   
     if ((localStorage.getItem("flutterWrapper") || 0) == 1 || (gv.setting?.device_setting?.use_server_network_printing||0)==1) { 
         var printers = (gv.setting?.device_setting?.station_printers).filter((e) => e.cashier_printer == 1);
         if (printers.length <= 0) {
