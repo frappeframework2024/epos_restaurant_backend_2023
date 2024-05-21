58fe5ae9c6ad7b1c76c3411bbbd691d300081645
outdomleap <outdom.leap@esoftix.com>
2024-05-21 11:32:32 +0700

    update

--- epos_restaurant_2023/www/product_detail.py
+++ epos_restaurant_2023/www/product_detail.py
@@ -3,6 +3,7 @@ from py_linq import Enumerable
 def get_context(context):
     context.no_cache = 1
     product = frappe.get_doc("Product",frappe.form_dict.product_code)
+    menu = frappe.get_doc("eMenu",frappe.form_dict.menu_id)
     min_price = product.price or 0
     max_price = product.price or 0
     
@@ -11,5 +12,6 @@ def get_context(context):
         max_price = Enumerable(product.product_price).max(lambda x: x.price or 0)
 
     context.doc = product
+    context.emenu = menu
     context.min_price = min_price
     context.max_price = max_price
