ebd622c5b7980e1459edb0854a8d71a8e774f321
outdomleap <outdom.leap@esoftix.com>
2024-05-24 16:00:18 +0700

    update emenu

--- epos_restaurant_2023/selling/doctype/emenu_popular_products/emenu_popular_products.py
+++ epos_restaurant_2023/selling/doctype/emenu_popular_products/emenu_popular_products.py
@@ -1,9 +1,10 @@
 # Copyright (c) 2023, Tes Pheakdey and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class eMenuPopularProducts(Document):
-	pass
+    pass
+ 
