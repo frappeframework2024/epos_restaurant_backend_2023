99cfaaaff607c5061254e2d23e4abb3930866a82
outdomleap <outdom.leap@esoftix.com>
2024-05-25 08:42:50 +0700

    up[date

--- epos_restaurant_2023/inventory/doctype/product/utils.py
+++ epos_restaurant_2023/inventory/doctype/product/utils.py
@@ -16,21 +16,32 @@ def update_fetch_from_field(self):
         updated_data["product_name_en"] = self.product_name_en
         data_for_updates.append({"doctype":"eMenu Popular Products","update_field":"product_name_en=%(product_name_en)s"})
         
- 
+    # if product name kh changed
     if self.has_value_changed("product_name_kh"):	
         updated_data["product_name_kh"] = self.product_name_kh
         data_for_updates.append({"doctype":"eMenu Popular Products","update_field":"product_name_kh=%(product_name_kh)s"})
+    
+    #if photo changed
+    if self.has_value_changed("photo"):
+        updated_data["photo"] = self.photo
+        data_for_updates.append({"doctype":"eMenu Popular Products","update_field":"photo=%(photo)s"})
         
+    # if description changed
     if self.has_value_changed("description"):	
         updated_data["description"] = self.description         
         data_for_updates.append({"doctype":"eMenu Popular Products","update_field":"description=%(description)s"})
+
+    # if is empty stock change
+    if self.has_value_changed("is_empty_stock_warning"):
+        updated_data["is_empty_stock_warning"] = self.is_empty_stock_warning         
+        data_for_updates.append({"doctype":"eMenu Popular Products","update_field":"is_empty_stock_warning=%(is_empty_stock_warning)s"})
         
-        
+    # if price is changed
     if self.has_value_changed("price"):	
         updated_data["price"] = self.price 
         data_for_updates.append({"doctype":"eMenu Popular Products","update_field":"price=%(price)s"})
         
-    
+    # if product_price changed
     if self.has_value_changed("product_price"):	
         prices = '[]'
         if self.product_price:
