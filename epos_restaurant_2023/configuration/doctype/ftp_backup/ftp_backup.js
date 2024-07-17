66cc41bee7ff8b95121b97767124524fa1ed704a
outdomleap <outdom.leap@esoftix.com>
2024-07-17 15:42:03 +0700

    update

--- epos_restaurant_2023/configuration/doctype/ftp_backup/ftp_backup.js
+++ epos_restaurant_2023/configuration/doctype/ftp_backup/ftp_backup.js
@@ -1,8 +1,13 @@
 // Copyright (c) 2023, Tes Pheakdey and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("FTP Backup", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("FTP Backup", {
+	run_backup(frm) {
+        frappe.call({
+            method: 'epos_restaurant_2023.api.ftp_backup.execute_backup_command', 
+            callback: function(r) { 
+               frappe.msgprint("Backup Added To Queue")
+            }
+        })
+	},
+});
