d3e63f80c4fad750c3e1400849b6a936c9f2d50d
frappeerpnext <outdom.leap@esoftix.com>
2024-08-12 09:33:50 +0700

    update

--- epos_restaurant_2023/configuration/doctype/business_branch/business_branch.js
+++ epos_restaurant_2023/configuration/doctype/business_branch/business_branch.js
@@ -18,5 +18,131 @@ frappe.ui.form.on("Business Branch", {
                 ]
             }
         });
+        frm.set_query("default_cash_account", function() {
+            return {
+                filters: [
+                    ["is_group","=","0"]
+                ]
+            }
+        });
+        frm.set_query("default_change_account", function() {
+            return {
+                filters: [
+                    ["is_group","=","0"]
+                ]
+            }
+        });
+        frm.set_query("default_bank_account", function() {
+            return {
+                filters: [
+                    ["is_group","=","0"]
+                ]
+            }
+        });
+        frm.set_query("default_pos_difference_amount_account", function() {
+            return {
+                filters: [
+                    ["is_group","=","0"]
+                ]
+            }
+        });
+        frm.set_query("default_cash_transaction_expense_account", function() {
+            return {
+                filters: [
+                    ["is_group","=","0"]
+                ]
+            }
+        });
+        frm.set_query("default_income_account", function() {
+            return {
+                filters: [
+                    ["is_group","=","0"]
+                ]
+            }
+        });
+        frm.set_query("default_receivable_account", function() {
+            return {
+                filters: [
+                    ["is_group","=","0"]
+                ]
+            }
+        });
+        frm.set_query("default_sale_discount_account", function() {
+            return {
+                filters: [
+                    ["is_group","=","0"]
+                ]
+            }
+        });
+        frm.set_query("default_sale_cash_coupon_claim_account", function() {
+            return {
+                filters: [
+                    ["is_group","=","0"]
+                ]
+            }
+        });
+        frm.set_query("default_tip_account", function() {
+            return {
+                filters: [
+                    ["is_group","=","0"]
+                ]
+            }
+        });
+        frm.set_query("default_credit_account", function() {
+            return {
+                filters: [
+                    ["is_group","=","0"]
+                ]
+            }
+        });
+        frm.set_query("default_purchase_discount_account", function() {
+            return {
+                filters: [
+                    ["is_group","=","0"]
+                ]
+            }
+        });
+        frm.set_query("income_head_account", function() {
+            return {
+                filters: [
+                    ["is_group","=","0"]
+                ]
+            }
+        });
+        frm.set_query("expense_head_account", function() {
+            return {
+                filters: [
+                    ["is_group","=","0"]
+                ]
+            }
+        });
+        frm.set_query("default_inventory_account", function() {
+            return {
+                filters: [
+                    ["is_group","=","0"]
+                ]
+            }
+        });
+        frm.set_query("stock_adjustment_account", function() {
+            return {
+                filters: [
+                    ["is_group","=","0"]
+                ]
+            }
+        });
+        frm.set_query("default_cost_of_good_sold_account", function() {
+            return {
+                filters: [
+                    ["is_group","=","0"]
+                ]
+            }
+        });
+        frm.set_query("default_bank_fee_account", function() {
+            return {
+                filters: [
+                    ["is_group","=","0"]
+                ]
+            }
+        });
 	},
 });
