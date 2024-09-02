1941d39c4334eb3c1337bd377c470baf86789354
frappeerpnext <outdom.leap@esoftix.com>
2024-09-02 08:40:14 +0700

    update

--- epos_restaurant_2023/selling/doctype/sale/sale.js
+++ epos_restaurant_2023/selling/doctype/sale/sale.js
@@ -474,7 +474,7 @@ frappe.ui.form.on("Sale", "refresh", function(frm) {
 frappe.ui.form.on('POS Sale Payment', {
 	payment_type(frm, cdt, cdn){
 		var d = frappe.model.get_doc(cdt, cdn);
-		// frappe.model.set_value(cdt, cdn, "input_amount", (frm.doc.grand_total * d.exchange_rate) - (frm.doc.total_paid || 0) - (frm.doc.total_cash_coupon_claim || 0) );
+		frappe.model.set_value(cdt, cdn, "input_amount", (frm.doc.grand_total * d.exchange_rate));
 	},
 
 	input_amount(frm) {
