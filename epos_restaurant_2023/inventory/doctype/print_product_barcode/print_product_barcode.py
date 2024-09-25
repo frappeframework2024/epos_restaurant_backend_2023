1d0cff8020d6c978d0a479fe492ddaeb6ddcbe9f
frappeerpnext <outdom.leap@esoftix.com>
2024-09-25 16:45:05 +0700

    update

--- epos_restaurant_2023/inventory/doctype/print_product_barcode/print_product_barcode.py
+++ epos_restaurant_2023/inventory/doctype/print_product_barcode/print_product_barcode.py
@@ -8,22 +8,25 @@ from frappe.model.document import Document
 class PrintProductBarcode(Document):
 	@frappe.whitelist(allow_guest=True)
 	def get_product_price(self):
-		cat = frappe.db.get_value('Product Category', self.category, ['lft', 'rgt'], as_dict=1)
-		products = frappe.db.sql("""WITH categories AS(
-									SELECT 
-										name 
-									FROM `tabProduct Category` 
-									where lft >= {0} and rgt <= {1})
+		if self.category == "" or self.category == None:
+			return []
+		else:
+			cat = frappe.db.get_value('Product Category', self.category, ['lft', 'rgt'], as_dict=1)
+			products = frappe.db.sql("""WITH categories AS(
+										SELECT 
+											name 
+										FROM `tabProduct Category` 
+										where lft >= {0} and rgt <= {1})
 
-									SELECT
-										coalesce(a.barcode,'Not Set') product_code,
-										a.price_rule,
-										a.unit,
-										a.price,
-						   				b.photo,
-										b.product_name_en,
-										b.product_name_kh
-									FROM `tabProduct Price` a
-									INNER JOIN `tabProduct` b ON b.name = a.parent
-									WHERE b.product_category IN(SELECT name FROM categories)""".format(cat.lft,cat.rgt),as_dict=1)
-		return products
+										SELECT
+											coalesce(a.barcode,'Not Set') product_code,
+											a.price_rule,
+											a.unit,
+											a.price,
+											b.photo,
+											b.product_name_en,
+											b.product_name_kh
+										FROM `tabProduct Price` a
+										INNER JOIN `tabProduct` b ON b.name = a.parent
+										WHERE b.product_category IN(SELECT name FROM categories)""".format(cat.lft,cat.rgt),as_dict=1)
+			return products
