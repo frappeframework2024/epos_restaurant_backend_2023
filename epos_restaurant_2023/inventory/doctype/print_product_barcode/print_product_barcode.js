879d31acc57fb456c4a3b5d34eda24da1464733a
frappeerpnext <outdom.leap@esoftix.com>
2024-09-25 16:28:57 +0700

    update

---
+++ epos_restaurant_2023/inventory/doctype/print_product_barcode/print_product_barcode.js
@@ -0,0 +1,50 @@
+// Copyright (c) 2024, Tes Pheakdey and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Print Product Barcode", {
+	refresh(frm) {
+        frm.set_query("category", function() {
+            return {
+                filters: [
+                    ["disabled","=",false]
+                ]
+            }
+        });
+	},
+    category(frm){
+        frappe.call({
+			method: 'get_product_price',
+			doc: frm.doc,
+			callback: (r) => {
+				if(r.message != null){
+                   set_child_table(frm)
+				}
+                frm.refresh_field('items');
+			}
+		})
+    }
+});
+function set_child_table(frm){
+    frm.set_value('items', []);
+    frm.refresh_field('items');
+    frappe.call({
+        method: 'get_product_price',
+        doc: frm.doc,
+        callback: (r) => {
+            if(r.message){
+                r.message.forEach(p => {
+                    frm.add_child('items', {
+                        product_code : p.product_code,
+                        product_name_en : p.product_name_en,
+                        product_name_kh : p.product_name_kh,
+                        price_rule : p.price_rule,
+                        unit : p.unit,
+                        price : p.price
+                    });
+                });
+            }
+        }
+    }).then((result)=>{
+        frm.refresh_field('items')
+    })
+}
---
+++ epos_restaurant_2023/inventory/doctype/print_product_barcode/print_product_barcode.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2024, Tes Pheakdey and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class PrintProductBarcode(Document):
+	@frappe.whitelist(allow_guest=True)
+	def get_product_price(self):
+		cat = frappe.db.get_value('Product Category', self.category, ['lft', 'rgt'], as_dict=1)
+		products = frappe.db.sql("""WITH categories AS(
+									SELECT 
+										name 
+									FROM `tabProduct Category` 
+									where lft >= {0} and rgt <= {1})
+
+									SELECT
+										coalesce(a.barcode,'Not Set') product_code,
+										a.price_rule,
+										a.unit,
+										a.price,
+										b.product_name_en,
+										b.product_name_kh
+									FROM `tabProduct Price` a
+									INNER JOIN `tabProduct` b ON b.name = a.parent
+									WHERE b.product_category IN(SELECT name FROM categories)""".format(cat.lft,cat.rgt),as_dict=1)
+		return products
