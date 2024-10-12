c2d5e940084d16515f957df1e53f076ebb55e7a1
frappeerpnext <outdom.leap@esoftix.com>
2024-10-12 11:31:55 +0700

    upate

--- epos_restaurant_2023/epos_restaurant/doctype/reset_data/reset_data.py
+++ epos_restaurant_2023/epos_restaurant/doctype/reset_data/reset_data.py
@@ -24,7 +24,22 @@ class ResetData(Document):
 			frappe.call('epos_restaurant_2023.install.reset_sale_transaction')
 		else:
 			frappe.msgprint("?")
-		pass
+		
+		naming_series = []
+		naming_series = frappe.db.sql("select distinct pos_bill_number_prefix as naming from `tabPOS Config`", as_dict=1)
+		naming_series = naming_series + frappe.db.sql("select distinct waiting_number_prefix as naming from `tabPOS Profile`",as_dict=1)
+		series =[]
+		for d in naming_series:
+			if d["naming"]:
+				string = d["naming"]
+				date = frappe.utils.nowdate()
+				year = date.split("-")[0]
+				year_short = year[-2:]
+				month = date.split("-")[1]
+				naming = string.replace('.', '').replace('YYYY', year).replace('yyyy', year).replace('YY', year_short).replace('yy', year_short).replace('MM', month).replace('#', '')
+				series.append(naming)
+		
+		frappe.db.sql("update `tabSeries` set current=  0 where name in %(format)s", {"format":[d for d in series]} )
 
 	def on_update(self):
 		frappe.db.sql("""update `tabReset Data` set stored_password = '' where docstatus = 1 and name = '{0}'""".format(self.name))
