b2a9c8640ddd7f388baf12de95827ff6a6a5e8c8
frappeerpnext <outdom.leap@esoftix.com>
2024-10-29 15:03:04 +0700

    update

--- epos_frontend/src/views/sale/AddSale.vue
+++ epos_frontend/src/views/sale/AddSale.vue
@@ -35,9 +35,9 @@
                     </div>
                     <div class="mt-auto">
                         <div class="-mx-1 bg-blue-100 rounded-tl-md rounded-tr-md text-xs">
-                            
-                            <ComSaleSummaryList />
                             <ComSaleKeyPad v-if="gv.device_setting.show_keypad_in_sale_screen == 1" />
+                            <ComSaleSummaryList />
+                            
                             <ComSaleButtonPaymentSubmit />
                         </div>
                     </div>
