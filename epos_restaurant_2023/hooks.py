5cf1c4ba9eb58db9afd5f2949990bf0e0dcdde40
frappeerpnext <outdom.leap@esoftix.com>
2025-03-08 09:03:53 +0700

    update

--- epos_restaurant_2023/hooks.py
+++ epos_restaurant_2023/hooks.py
@@ -80,7 +80,6 @@ doctype_list_js = {"Translation": "public/js/translation_list.js"}
 before_migrate ="epos_restaurant_2023.migrate.before_migrate" 
 after_migrate = [
     "epos_restaurant_2023.migrate.after_migrate",
-    "epos_restaurant_2023.store_procedures.stored_procedure.execute"
     ]
  
 # Uninstallation
