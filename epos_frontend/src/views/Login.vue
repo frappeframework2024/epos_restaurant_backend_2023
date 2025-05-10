c70a0b5d06bf72c935fc77c829184d1ba1387f08
frappeerpnext <outdom.leap@esoftix.com>
2025-05-10 11:18:09 +0700

    update

--- epos_frontend/src/views/Login.vue
+++ epos_frontend/src/views/Login.vue
@@ -102,8 +102,7 @@
                 <div class="mt-2">
                   <v-btn size="x-large" class="w-full" color="light" @click="(() => {
                     $i18n.locale = ($i18n.locale == 'kh' ? 'en' : 'km');
-                    onChangeLang($i18n.locale)
-
+                    onChangeLang($i18n.locale, $i18n.locale == 'en' ? 'English' : 'Khmer')
                   })">{{ $i18n.locale == "en" ? "ខ្មែរ" : "English" }}</v-btn>
                 </div>
               </template>
