19d6372156b9c90723cf6b412227d324a94e41a0
frappeerpnext <outdom.leap@esoftix.com>
2025-06-07 10:19:33 +0700

    update

--- epos_restaurant_2023/selling/doctype/cashier_shift/cashier_shift.py
+++ epos_restaurant_2023/selling/doctype/cashier_shift/cashier_shift.py
@@ -26,9 +26,9 @@ class CashierShift(Document):
 			exchange_rate = exchange_rate or 1
 			c.exchange_rate = exchange_rate
 			
-			c.opening_amount = float(c.input_amount) / exchange_rate
-			c.close_amount = float(c.input_close_amount) / exchange_rate
-			c.system_close_amount = float(c.input_system_close_amount) / exchange_rate
+			c.opening_amount = (float(c.input_amount) if c.input_amount.strip() else 0) / exchange_rate
+			c.close_amount = float(c.input_close_amount) if c.input_close_amount.strip() else 0
+			c.system_close_amount = (float(c.input_system_close_amount) if c.input_system_close_amount.strip() else 0) / exchange_rate
 
 			c.different_amount = (c.close_amount or 0) - (c.system_close_amount or 0)
 
