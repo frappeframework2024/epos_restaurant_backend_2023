51a997aefa47bb96b36ae27ad93efc45dea249fa
frappeerpnext <outdom.leap@esoftix.com>
2025-09-08 14:36:20 +0700

    update

--- epos_frontend/src/views/sale/components/ComSplitBill.vue
+++ epos_frontend/src/views/sale/components/ComSplitBill.vue
@@ -127,6 +127,7 @@ function onCreateNew() {
   _sale.name = "";
   _sale.seat_number = 0;
   _sale.custom_bill_number = undefined;
+  _sale.deposit = 0;
   if (_deleted.length > 0) {
     _sale = JSON.parse(JSON.stringify(_deleted[0].sale));
     _deleted[0].sale.name = "";
