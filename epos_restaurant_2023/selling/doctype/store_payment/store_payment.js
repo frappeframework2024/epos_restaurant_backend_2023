88d63b6aa64c032203c2703bbbd684689182db22
frappeerpnext <outdom.leap@esoftix.com>
2025-09-25 17:25:39 +0700

    update

--- epos_restaurant_2023/selling/doctype/store_payment/store_payment.js
+++ epos_restaurant_2023/selling/doctype/store_payment/store_payment.js
@@ -3,9 +3,10 @@
 
 frappe.ui.form.on("Store Payment", {
 	business_branch(frm) {
-		change_branch(frm)
+		// change_branch(frm)
 	},
-	vendor(frm) {
+	pos_profile(frm) {
+		alert(frm.doc.pos_profile)
 		frappe.call({
 			method: 'epos_restaurant_2023.selling.doctype.store_payment.store_payment.get_vendor_credit_balance',
 			args: {
@@ -15,36 +16,11 @@ frappe.ui.form.on("Store Payment", {
 				frm.set_value("credit_amount", r.message.balance)
 			}
 		})
-	},
-	pos_profile(frm) {
-		
 	}
 });
 
 frappe.ui.form.on('Store Payment Type', {
-    payment_type(frm,cdt,cdn){
-        if(frm.doc.business_branch == "" || frm.doc.business_branch == null){
-			frappe.throw("Please Select Business Branch First")
-			return
-		}
-		let doc = locals[cdt][cdn];
-		frappe.call({
-			method: 'epos_restaurant_2023.selling.doctype.store_payment.store_payment.get_payment_type_account',
-			args: {
-				payment_type: doc.payment_type,
-				branch: frm.doc.business_branch
-			},
-			callback: (r) => {
-				if(r.message && r.message != "no_record"){
-					frappe.model.set_value(cdt, cdn, "account_code", (r.message[0].account || ""));
-                    frappe.model.set_value(cdt, cdn, "exchange_rate", (r.message[0].exchange_rate || 1));
-				}
-			},
-			error: (r) => {
-				reject(r)
-			}
-		})
-    },
+     
     input_amount(frm,cdt,cdn){
         let doc = locals[cdt][cdn];
         frappe.model.set_value(cdt, cdn, "payment_amount", (doc.input_amount / doc.exchange_rate));
