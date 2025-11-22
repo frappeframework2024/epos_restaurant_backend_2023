220f409bb093781061ab80bf762f2b58b1fc6d55
frappeerpnext <outdom.leap@esoftix.com>
2025-11-22 14:20:19 +0700

    update

--- epos_restaurant_2023/configuration/doctype/printer/printer_list.js
+++ epos_restaurant_2023/configuration/doctype/printer/printer_list.js
@@ -1,5 +1,11 @@
 frappe.listview_settings['Printer'] = {
     onload: function(listview) {
+        frappe.realtime.on("update_printer_to_products", (data) => {
+            frappe.show_alert({
+                message: data.message,
+                indicator: 'blue'
+            });
+        });
         listview.page.add_inner_button(__('Update Printer to Products'), function() {
             frappe.confirm(
                 'Are you sure you want process this transaction?',
