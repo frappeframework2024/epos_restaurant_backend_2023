f58427e9436854c1dd693a21a0622ebb28dff76b
frappeerpnext <outdom.leap@esoftix.com>
2025-12-05 09:39:40 +0700

    update

--- epos_restaurant_2023/employee_management/doctype/employee_commission_payment/employee_commission_payment.py
+++ epos_restaurant_2023/employee_management/doctype/employee_commission_payment/employee_commission_payment.py
@@ -22,19 +22,23 @@ class EmployeeCommissionPayment(Document):
 	@frappe.whitelist()
 	def get_sale_commission(doc):
 		sales=[]
+		outlet_filter = ""
+		if doc.outlet:
+			outlet_filter = "  and  outlet = '{0}'".format(doc.outlet)
 		data = frappe.db.sql("""
 					   select name,
 					   sale_commission_amount,
 					   sale_commission_paid_amount,
 					   sale_commission_balance,
-					   posting_date
+					   posting_date,
+					   outlet
 					   from `tabSale` 
 					   where sale_commission_to = '{0}' and 
 					   sale_commission_balance > 0 and docstatus = 1 and
-					   posting_date between '{1}' and '{2}'
-					   """.format(doc.employee,doc.sale_start_date,doc.sale_end_date),as_dict=1)
+					   posting_date between '{1}' and '{2}' {3}
+					   """.format(doc.employee,doc.sale_start_date,doc.sale_end_date,outlet_filter),as_dict=1)
 		for a in data:
-			sales.append({"sale":a.name,"commission_amount":a.sale_commission_amount,"paid_amount":a.sale_commission_paid_amount,"balance":a.sale_commission_balance,"posting_date":a.posting_date})
+			sales.append({"sale":a.name,"commission_amount":a.sale_commission_amount,"paid_amount":a.sale_commission_paid_amount,"balance":a.sale_commission_balance,"posting_date":a.posting_date,"outlet":a.outlet})
 		if len(data) == 0:
 			sales = []
 		else:
