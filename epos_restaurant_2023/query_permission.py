91fb72ad168f4cf0baa6d06accf7e24b0ea0cded
frappeerpnext <outdom.leap@esoftix.com>
2025-12-10 09:50:15 +0700

    update

--- epos_restaurant_2023/query_permission.py
+++ epos_restaurant_2023/query_permission.py
@@ -3,5 +3,5 @@ def get_employee_permission(user):
     if user != "Administrator":
         return "(`tabEmployee`.employee_name not in ('Pheakdey','Test'))"
 
-def product_filters(user):
+def product_filters():
     return "(`tabProduct`.status not in ('Variant','Disabled'))"
