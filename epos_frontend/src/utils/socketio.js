ee993917138ae6e76d2be4121e680c91d9258f63
frappeerpnext <outdom.leap@esoftix.com>
2026-05-28 16:33:40 +0700

    update

--- epos_frontend/src/utils/socketio.js
+++ epos_frontend/src/utils/socketio.js
@@ -1,21 +1,8 @@
-
-
 import io from 'socket.io-client';
-import { websocket_port } from "../../../../../sites/common_site_config.json"
 
-let host = window.location.hostname;
-let protocol = window.location.protocol;
-let port ="";
-
-if (protocol=="http:"){
-    port = ":" +  websocket_port;
-}else  {
-    port = ":" +  window.location.port;
-}
- 
-
- 
-let socket = io(protocol + "//" + host + port, { path: '/socketserver/socket.io' });
+const socket = io(window.location.origin, {
+    path: '/socketserver/socket.io',
+    transports: ['websocket', 'polling']
+});
 
 export default socket;
-
