2fd614ee117bd75bc4330582f9c9b71c0cbcd6c6
frappeerpnext <outdom.leap@esoftix.com>
2026-08-01 08:52:46 +0700

    update epos

--- epos_restaurant_2023/configuration/doctype/pos_profile/pos_profile.py
+++ epos_restaurant_2023/configuration/doctype/pos_profile/pos_profile.py
@@ -5,4 +5,8 @@
 from frappe.model.document import Document
 
 class POSProfile(Document):
-	pass
+	def on_update(self):
+		frappe.clear_document_cache('POS Profile', self.name)
+		frappe.cache().delete_keys(f"api.sale.get_default_print_template::{self.pos_config}")
+		frappe.cache.delete_keys("system_settings:*")
+
